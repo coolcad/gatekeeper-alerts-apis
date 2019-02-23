fd05a3d4d4b438553e6b0816670e65718548cc43
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-02-22 20:11:44 -0500

    can send array of alerts

---
+++ src/routes/apiRoutes/validators/index.js
@@ -0,0 +1,42 @@
+const { celebrate, Joi, errors } = require("celebrate");
+
+const validators = {};
+
+validators.alert = () => {
+  return celebrate({
+    body: Joi.array().items(
+      Joi.object().keys({
+        alertName: Joi.string().required(),
+        alertMessage: Joi.string().required(),
+        deliveryMethods: Joi.array()
+          .items(Joi.string().valid("sms", "email"))
+          .required(),
+        alertLogs: Joi.array().items(
+          Joi.object().keys({
+            alertDateTime: Joi.date()
+              .iso()
+              .required(),
+            eventType: Joi.string().required(),
+            computerName: Joi.string().required(),
+            userName: Joi.string()
+          })
+        ),
+        receivers: Joi.array()
+          .items(
+            Joi.object().keys({
+              name: Joi.string(),
+              email: Joi.string()
+                .email()
+                .required(),
+              phoneNumber: Joi.string()
+                .allow(null)
+                .optional()
+            })
+          )
+          .required()
+      })
+    )
+  });
+};
+
+module.exports = validators;
