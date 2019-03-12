4fd6c313ba914f83d2b5f512689e5bf47d33e7d8
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-03-12 16:06:14 -0400

    remove pre push hook until jest openhandle issue fix

--- __tests__/routes/v1.test.js
+++ __tests__/routes/v1.test.js
@@ -11,21 +11,23 @@ const v1Routes = {
 };
 
 describe("Application is responsive", () => {
-  it("GET /", async () => {
+  it("GET /", async done => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
+    done();
   });
 });
 
 describe("Invalid Route is 404", () => {
-  it("GET /doesnotexist", async () => {
+  it("GET /doesnotexist", async done => {
     const response = await request(app).get("/doesnotexist");
     expect(response.statusCode).toBe(404);
+    done();
   });
 });
 
 describe("POST /api/v1/alerts/send", () => {
-  beforeEach(async () => {
+  beforeAll(async done => {
     await mongoose.connect(config.databaseUri, {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -34,11 +36,14 @@ describe("POST /api/v1/alerts/send", () => {
       reconnectTries: 30000,
       poolSize: 10
     });
+    done();
   });
-  afterEach(async () => {
+  afterAll(async done => {
     await mongoose.connection.close();
+
+    done();
   });
-  it("should fail with req body validation", async () => {
+  it("should fail with req body validation", async done => {
     const response = await request(app)
       .post(v1Routes.alerts.send)
       .send({
@@ -48,9 +53,10 @@ describe("POST /api/v1/alerts/send", () => {
       });
 
     expect(response.status).toBe(400);
+    done();
   });
 
-  it.only("should throw error for wrong Authorization header", async () => {
+  it.only("should throw error for wrong Authorization header", async done => {
     const response = await request(app)
       .post(v1Routes.alerts.send)
       .set("Authorization", "c3eba8b4-4dde-49fa-9c4b-e138118f5a8a") // Its a wrong externalLicenseId
@@ -77,9 +83,10 @@ describe("POST /api/v1/alerts/send", () => {
       ]);
 
     expect(response.status).toBe(400);
+    done();
   });
 
-  it.skip("should send an email", async () => {
+  it.skip("should send an email", async done => {
     const response = await request(app)
       .post(v1Routes.alerts.send)
       .set("Authorization", "c3eba8b4-4dde-49fa-9c4b-e138118f5a8d")
@@ -106,9 +113,10 @@ describe("POST /api/v1/alerts/send", () => {
       ]);
 
     expect(response.status).toBe(200);
+    done();
   });
 
-  it.skip("should send an sms", async () => {
+  it.skip("should send an sms", async done => {
     const response = await request(app)
       .post(v1Routes.alerts.send)
       .set("Authorization", "c3eba8b4-4dde-49fa-9c4b-e138118f5a8d")
@@ -136,5 +144,6 @@ describe("POST /api/v1/alerts/send", () => {
       ]);
 
     expect(response.status).toBe(200);
+    done();
   });
 });
